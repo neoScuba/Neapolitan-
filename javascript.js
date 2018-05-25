8d5171280be8524a01c1855b731192713e4a4046
LunaPC <lunad3@uci.edu>
2018-05-24 21:52:44 -0700

    console prints out users input

---
+++ javascript.js
@@ -0,0 +1,11 @@
+$("#submitBtn").on("click",function(){
+    var state = $("#stateId").val();
+    var city = $("#cityId").val();
+    var zip = $("#zipId").val();
+    var street = $("#streetId").val();
+
+    console.log(state);
+    console.log(city);
+    console.log(zip);
+    console.log(street);
+});
