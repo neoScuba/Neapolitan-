e642db7bb87e6c75bccbd59abf7c6a1277790887
DanielLuna <lunad3@uci.edu>
2018-05-26 18:01:28 -0700

    using google map API, console log list of address_components using only zip

--- javascript.js
+++ javascript.js
@@ -1,39 +1,46 @@
 var apiKey = "AIzaSyBB5fWaIK-L-cV2wNpc2G2cQRBtQlRR4B4";
 var baseUrl = "https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyBB5fWaIK-L-cV2wNpc2G2cQRBtQlRR4B4&address="
 //https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyBB5fWaIK-L-cV2wNpc2G2cQRBtQlRR4B4&address=1263%20Pacific%20Ave.%20Kansas%20City%20KS&electionId=2000   
-
+// ocd-division/country:us/state:nc/county:wake
+//http://maps.googleapis.com/maps/api/geocode/json?address=92612&sensor=true
 $("#submitBtn").on("click", function () {
-    var state = $("#stateId").val().trim();
-    var city = $("#cityId").val().trim();
     var zip = $("#zipId").val().trim();
-    var street = $("#streetId").val().trim();
+    var ocid = zip2Ocid(zip);
+    // var candidateList = ocid2CandidateList(ocid);
+});
 
-    var address = street + "%20" + state + "%20" + city + "%20" + zip;
-    address = address.replace(/ /g, "%20");
-
-    $("#stateId").val("");
-    $("#cityId").val("");
-    $("#zipId").val("");
-    $("#streetId").val("");
-
-    var Url = baseUrl + address;
+function zip2Ocid (potentialZip){
+        if (potentialZip.length == 5 && !isNaN(potentialZip)){
+            $.ajax({
+                url: "http://maps.googleapis.com/maps/api/geocode/json?address="+potentialZip+"&sensor=true",
+                method: 'GET',
+            }).done(function (response) {
+                console.log("zip2CityCounty::done:: Valid zip")
+                console.log(response.results["0"].address_components);
+            }).fail(function (response) {
+                console.log("zip2CityCounty::done:: Not valid zip");
+            })
+        
+        }
+    };
+function ocid2CandidateList(ocid){
 
     $.ajax({
         url: Url,
         method: 'GET',
     }).done(function (response) {
-        console.log("SbmitBtn::onClick::ajax::done:: Valid Address")
-        var candidateList = response.contests[0].candidates;
-        var office = response.contests[0].office;
-        for (var candidateIndex = 0; candidateIndex < candidateList.length; candidateIndex++) {
-            var candidate = $("<a class='collection-item candidate'></a>");
-            $(candidate).text(candidateList[candidateIndex].name);
-            $("#candidateListId").append(candidate);
-        }
+        console.log("ocid2CandidateList::ajax::done")
+        console.log(response);
     }).fail(function (response) {
-        console.log("SbmitBtn::onClick::ajax::fail:: Invalid address");
+        console.log("ocid2CandidateList::ajax::fail");
     })
-});
+};
+
+
+
+
+
+
 var searchTerm = "Apple";
 
     $('#search').on('click', function () {
