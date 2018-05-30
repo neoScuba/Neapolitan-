f60276a776770a689c6474f018640f778ec9195f
DanielLuna <lunad3@uci.edu>
2018-05-29 18:32:30 -0700

    list current repBtns

--- javascript.js
+++ javascript.js
@@ -1,15 +1,9 @@
 var apiKey = "AIzaSyBB5fWaIK-L-cV2wNpc2G2cQRBtQlRR4B4";
-var baseUrl = "https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyBB5fWaIK-L-cV2wNpc2G2cQRBtQlRR4B4&address="
-//https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyBB5fWaIK-L-cV2wNpc2G2cQRBtQlRR4B4&address=92612
-// ocd-division/country:us/state:nc/county:wake
-// https://www.googleapis.com/civicinfo/v2/representatives/ocd-division%2Fcountry%3Aus%2Fstate%3Anc%2Fcounty%3Awake?key=AIzaSyBB5fWaIK-L-cV2wNpc2G2cQRBtQlRR4B4
-//http://maps.googleapis.com/maps/api/geocode/json?address=92612&sensor=true
-$("#submitBtn").on("click", function () {
-    var zip = $("#zipId").val().trim();
-    var ocdid = zip2ocdid(zip);
-    var candidateList = ocdid2CandidateList(ocdid);
-});
+var buttonDiv = "#candidateListId";
 
+//-------------------------FUNCTIONS-------------------------
+
+//------ zip2ocdid: if zip is valid turn it to an ocidid using map api, return ocdid
 function zip2ocdid (potentialZip){
     var ocdid = false;
     if (potentialZip.length == 5 && !isNaN(potentialZip)){
@@ -18,9 +12,9 @@ function zip2ocdid (potentialZip){
             method: 'GET',
             async:false,
         }).done(function (response) {
-            console.log("zip2CityCounty::done:: Valid zip")
-            console.log(response.results);
+            console.log("zip2CityCounty::done::Valid zip")
             var locationInfo = response.results["0"].address_components;
+            //we can grab the county and display it somewhere
             var country = locationInfo["4"].short_name.toLowerCase();
             var state   = locationInfo["3"].short_name.toLowerCase();
             var city  = locationInfo["1"].short_name.toLowerCase();
@@ -28,42 +22,55 @@ function zip2ocdid (potentialZip){
             ocdid = ocdid.replace(/:/g,"%3A");
             ocdid = ocdid.replace(/\//ig, "%2F");
             ocdid = ocdid.replace(" ", "_");
-            console.log(ocdid);
+            $(buttonDiv).empty();
         })
     }
     return ocdid;
 };
 
+//------ ocdid2CandidateList: uses ocdid to make civic api request, returns repList
 function ocdid2CandidateList(ocdid){
+    var repList;
     $.ajax({
         url: "https://www.googleapis.com/civicinfo/v2/representatives/"+ocdid+"?key="+apiKey,
         method: 'GET',
         async:false,
     }).done(function (response) {
-        console.log("ocdid2CandidateList::ajax::done")
-        console.log(response);
+        console.log("ocdid2CandidateList::ajax::done::Valid Ocdid");
+        repList = response.officials;
     }).fail(function (response) {
-        console.log("ocdid2CandidateList::ajax::fail");
+        console.log("ocdid2CandidateList::ajax::fail::Invalid Ocdid");
     })
+    return repList;
 };
 
+//------ displayRepList: turns list items {repObj} to buttons and appends to buttonDiv
+function displayRepList(repList){
+    for(var repIndex = 0; repIndex < repList.length; repIndex++){
+        // console.log(repList[repIndex]);
+        var repBtn = makeBtn(repList[repIndex]);
+        $(buttonDiv).append($(repBtn));
+    }
+};
 
+//------ makeBtn: turns {repObj} to button, returns button
+function makeBtn(repObject){
+    var btn = $("<button type='button'></button>");
+    $(btn).text(repObject.name);
+    $(btn).attr("class","representative");
+    return btn;
+};
 
-
-
-
-var searchTerm = "Apple";
-
+//------ wikiSearch: 
+function wikiSearch(searchTerm){
     $('#search').on('click', function () {
-        var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchTerm + "&format=json&callback=?";
         $.ajax({
-            url: url,
+            url: 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchTerm + "&format=json&callback=?",
             type: 'GET',
             dataType: 'json',
             data: function (data, status, jqXHR) {
-                console.log(data)
+                console.log(data);
             },
-
         })
         .done(function(response) {
             console.log(response);
@@ -75,3 +82,18 @@ var searchTerm = "Apple";
             console.log('complete');
         });
     });
+};
+
+//-------------------------ON.CLICK CALLS-------------------------
+
+$("#submitBtn").on("click", function () {
+    var zip = $("#zipId").val().trim();
+    var ocdid = zip2ocdid(zip);
+    var representativeList = ocdid2CandidateList(ocdid);
+    displayRepList(representativeList);
+});
+
+$(".representative").on("click",function(){
+    console.log("searching:" +$(this).text());
+    // wikiSearch($(this).text());
+});
